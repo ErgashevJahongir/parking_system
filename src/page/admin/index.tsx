0ad9301170df145db8c73f80f829f357d4f27b8f
Jahongir <jahongirergawev2@gmail.com>
2024-11-29 09:53:49 +0500

    some change for admin

--- src/page/admin/index.tsx
+++ src/page/admin/index.tsx
@@ -16,7 +16,7 @@ import {
 } from "@tanstack/react-table";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { ChevronDownIcon, HandCoins, Loader, Pencil, Plus, Search, Trash2 } from "lucide-react";
-import { useEffect, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import {
   AlertDialog,
   AlertDialogAction,
@@ -29,7 +29,7 @@ import {
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { useDelete, useList, useListParking } from "./services";
-import { IClient } from "./index.type";
+import { IClient, IClientListResponse } from "./index.type";
 import { loadState } from "@/utils/storage";
 import {
   DropdownMenu,
@@ -46,6 +46,7 @@ import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from "
 import dayjs from "dayjs";
 import LoadingComp from "@/components/loadingComp";
 import { IParking } from "../parkings/index.type";
+import { QueryObserverResult, RefetchOptions } from "@tanstack/react-query";
 
 interface IPageFilter {
   page: number;
@@ -60,6 +61,181 @@ export default function ClientPage() {
     size: loadState("table-limit") || 10,
     searchValue: null,
   });
+  const { data, refetch } = useList(pageFilter?.page, pageFilter?.size, pageFilter.searchValue);
+  const [addSheetOpen, setAddSheetOpen] = useState(false);
+  const [editSheetOpen, setEditSheetOpen] = useState(false);
+  const [editId, setEditId] = useState<string | null>(null);
+  const [payMonitoringSheetOpen, setPayMonitoringSheetOpen] = useState(false);
+  const [userId, setUserId] = useState<string | null>(null);
+  const { data: userParkings, isLoading: userParkingsLoading } = useListParking(pageFilter?.page, pageFilter?.size, null, userId as string, !!userId);
+
+  const columnsParking: ColumnDef<IParking>[] = [
+    {
+      accessorKey: "id",
+      meta: t("id"),
+      header: t("id"),
+    },
+    {
+      accessorKey: "type",
+      meta: t("Turi"),
+      header: t("Turi"),
+      cell: ({ row }) => <p className="whitespace-nowrap">{row.getValue("type")}</p>,
+    },
+    {
+      accessorKey: "car_number",
+      meta: t("Mashina raqami"),
+      header: t("Mashina raqami"),
+      cell: ({ row }) => <p className="whitespace-nowrap">{row.getValue("car_number")}</p>,
+    },
+    {
+      accessorKey: "start_time",
+      meta: t("Kirish vaqti"),
+      header: t("Kirish vaqti"),
+      cell: ({ row }) => <p className="whitespace-nowrap">{dayjs(row.getValue("start_time")).format("YYYY-MM-DD HH:mm:ss")}</p>,
+    },
+    {
+      accessorKey: "end_time",
+      meta: t("Chiqish vaqti"),
+      header: t("Chiqish vaqti"),
+      cell: ({ row }) => <p className="whitespace-nowrap">{(row.getValue("end_time")) ? dayjs(row.getValue("end_time")).format("YYYY-MM-DD HH:mm:ss") : ""}</p>,
+    },
+    {
+      accessorKey: "summ",
+      meta: t("Summa"),
+      header: t("Summa"),
+      cell: ({ row }) => <p className="whitespace-nowrap">{row.getValue("summ")}</p>,
+    },
+  ];
+
+  const tableParking = useReactTable({
+    data: userParkings?.reservations || [],
+    columns: columnsParking,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+
+  return (
+    <section>
+      <div className="flex items-center justify-between gap-3 mb-2">
+        <h2 className="font-semibold text-loginTitle">
+          Mijozlar
+          <sup className="text-mediumParagraph ml-1.5">
+            {data?.count && data?.count > 0 && data?.count}
+          </sup>
+        </h2>
+        <Button onClick={() => setAddSheetOpen(true)} variant="outline">
+          <Plus className="w-4 h-4 mr-2" />
+          Mijoz qo'shish
+        </Button>
+      </div>
+      <TableComp
+        data={data?.clients}
+        setPageFilter={setPageFilter}
+        refetch={refetch}
+        setEditId={setEditId}
+        setEditSheetOpen={setEditSheetOpen}
+        setUserId={setUserId}
+        setPayMonitoringSheetOpen={setPayMonitoringSheetOpen}
+      />
+      <Sheet
+        open={payMonitoringSheetOpen}
+        onOpenChange={(open) => {
+          setPayMonitoringSheetOpen(open);
+          !open && setUserId(null);
+        }}
+      >
+        <SheetContent
+          className="md:max-w-[700px] lg:max-w-[900px]"
+          side="right"
+        >
+          <SheetDescription className="hidden">hide</SheetDescription>
+          <SheetHeader>
+            <SheetTitle className="text-left">Mijoz parkovkalari</SheetTitle>
+          </SheetHeader>
+          {userParkingsLoading ? (
+            <div className="h-[200px] flex items-center justify-center">
+              <LoadingComp />
+            </div>
+          ) : (
+            <div className="grid gap-4 py-4">
+              <div>
+                <div className="relative max-h-[70vh] w-full overflow-y-auto rounded-lg border">
+                  <Table>
+                    <TableHeader>
+                      {tableParking.getHeaderGroups().map((headerGroup) => (
+                        <TableRow key={headerGroup.id}>
+                          {headerGroup.headers.map((header) => {
+                            return (
+                              <TableHead className="sticky top-0 z-[1]" key={header.id}>
+                                {header.isPlaceholder
+                                  ? null
+                                  : flexRender(header.column.columnDef.header, header.getContext())}
+                              </TableHead>
+                            );
+                          })}
+                        </TableRow>
+                      ))}
+                    </TableHeader>
+                    <TableBody className="w-full">
+                      {tableParking.getRowModel().rows?.length ? (
+                        tableParking.getRowModel().rows.map((row) => {
+                          return (
+                            <TableRow
+                              key={row.id}
+                              data-state={row.getIsSelected() && "selected"}
+                            >
+                              {row.getVisibleCells().map((cell) => {
+                                return (
+                                  <TableCell key={cell.id}>
+                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                  </TableCell>
+                                );
+                              })}
+                            </TableRow>
+                          )
+                        })
+                      ) : (
+                        <TableRow className="h-[62vh] w-full">
+                          <TableCell rowSpan={5} colSpan={columnsParking.length} className="h-24 text-center">
+                            Ma'lumot topilmadi.
+                          </TableCell>
+                        </TableRow>
+                      )}
+                    </TableBody>
+                  </Table>
+                </div>
+              </div>
+            </div>
+          )}
+        </SheetContent>
+      </Sheet>
+      <ClientCreate
+        setSheetOpen={setAddSheetOpen}
+        sheetOpen={addSheetOpen}
+        refetch={refetch}
+      />
+      <ClientUpdate
+        setSheetOpen={setEditSheetOpen}
+        sheetOpen={editSheetOpen}
+        refetch={refetch}
+        id={editId}
+        setEditId={setEditId}
+      />
+    </section>
+  );
+}
+
+const TableComp = ({ setPageFilter, refetch, data, setPayMonitoringSheetOpen, setUserId, setEditSheetOpen, setEditId }: {
+  setPageFilter: Dispatch<SetStateAction<IPageFilter>>,
+  refetch: (options?: RefetchOptions) => Promise<QueryObserverResult<IClientListResponse, Error>>,
+  data?: IClient[],
+  setEditId: Dispatch<SetStateAction<string | null>>,
+  setPayMonitoringSheetOpen: Dispatch<SetStateAction<boolean>>,
+  setUserId: Dispatch<SetStateAction<string | null>>,
+  setEditSheetOpen: Dispatch<SetStateAction<boolean>>
+}) => {
+  const { t } = useTranslation();
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
   const debouncedSearch = useDebounce((newQuery: string) => {
     refetch();
     setPageFilter((prev) => ({
@@ -69,18 +245,13 @@ export default function ClientPage() {
     }));
   }, 1000);
   const { mutate, isPending: isLoadingDelete } = useDelete();
-  const {
-    data: tableData,
-    // isLoading,
-    refetch,
-  } = useList(pageFilter?.page, pageFilter?.size, pageFilter.searchValue);
-  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
-  const [addSheetOpen, setAddSheetOpen] = useState(false);
-  const [editSheetOpen, setEditSheetOpen] = useState(false);
-  const [editId, setEditId] = useState<string | null>(null);
-  const [payMonitoringSheetOpen, setPayMonitoringSheetOpen] = useState(false);
-  const [userId, setUserId] = useState<string | null>(null);
-  const { data: userParkings, isLoading: userParkingsLoading } = useListParking(pageFilter?.page, pageFilter?.size, null, userId as string, !!userId);
+
+  useEffect(() => {
+    const storedData = JSON.parse(window.localStorage.getItem("columnVisibilityClient") || "{}")
+    if (storedData) {
+      setColumnVisibility(storedData)
+    }
+  }, [])
 
   const handleDelete = (id: string) => {
     mutate(id, {
@@ -93,13 +264,6 @@ export default function ClientPage() {
       },
     });
   };
-
-  useEffect(() => {
-    const storedData = JSON.parse(window.localStorage.getItem("columnVisibilityClient") || "{}")
-    if (storedData) {
-      setColumnVisibility(storedData)
-    }
-  }, [])
 
   const columns: ColumnDef<IClient>[] = [
     {
@@ -192,7 +356,7 @@ export default function ClientPage() {
   ];
 
   const table = useReactTable({
-    data: tableData?.clients || [],
+    data: data || [],
     columns,
     getCoreRowModel: getCoreRowModel(),
     onColumnVisibilityChange: setColumnVisibility,
@@ -201,68 +365,8 @@ export default function ClientPage() {
     },
   });
 
-  const columnsParking: ColumnDef<IParking>[] = [
-    {
-      accessorKey: "id",
-      meta: t("id"),
-      header: t("id"),
-    },
-    {
-      accessorKey: "type",
-      meta: t("Turi"),
-      header: t("Turi"),
-      cell: ({ row }) => <p className="whitespace-nowrap">{row.getValue("type")}</p>,
-    },
-    {
-      accessorKey: "car_number",
-      meta: t("Mashina raqami"),
-      header: t("Mashina raqami"),
-      cell: ({ row }) => <p className="whitespace-nowrap">{row.getValue("car_number")}</p>,
-    },
-    {
-      accessorKey: "start_time",
-      meta: t("Kirish vaqti"),
-      header: t("Kirish vaqti"),
-      cell: ({ row }) => <p className="whitespace-nowrap">{dayjs(row.getValue("start_time")).format("YYYY-MM-DD HH:mm:ss")}</p>,
-    },
-    {
-      accessorKey: "end_time",
-      meta: t("Chiqish vaqti"),
-      header: t("Chiqish vaqti"),
-      cell: ({ row }) => <p className="whitespace-nowrap">{(row.getValue("end_time")) ? dayjs(row.getValue("end_time")).format("YYYY-MM-DD HH:mm:ss") : ""}</p>,
-    },
-    {
-      accessorKey: "summ",
-      meta: t("Summa"),
-      header: t("Summa"),
-      cell: ({ row }) => <p className="whitespace-nowrap">{row.getValue("summ")}</p>,
-    },
-  ];
-
-  const tableParking = useReactTable({
-    data: userParkings?.reservations || [],
-    columns: columnsParking,
-    getCoreRowModel: getCoreRowModel(),
-    onColumnVisibilityChange: setColumnVisibility,
-    state: {
-      columnVisibility,
-    },
-  });
-
   return (
-    <section>
-      <div className="flex items-center justify-between gap-3 mb-2">
-        <h2 className="font-semibold text-loginTitle">
-          Mijozlar
-          <sup className="text-mediumParagraph ml-1.5">
-            {tableData?.count && tableData?.count > 0 && tableData?.count}
-          </sup>
-        </h2>
-        <Button onClick={() => setAddSheetOpen(true)} variant="outline">
-          <Plus className="w-4 h-4 mr-2" />
-          Mijoz qo'shish
-        </Button>
-      </div>
+    <>
       <div className="flex flex-col items-center justify-between gap-3 mb-3 sm:flex-row md:mb-4">
         <form className="w-full">
           <div className="relative">
@@ -278,7 +382,7 @@ export default function ClientPage() {
           </div>
         </form>
         <div className="flex items-center w-full gap-3 md:w-auto">
-          <DropdownMenu>
+          <DropdownMenu modal={false}>
             <DropdownMenuTrigger asChild>
               <div className={cn(buttonVariants({ variant: "outline", className: "ml-auto" }))}>
                 Ustunlarni sozlash <ChevronDownIcon className="w-4 h-4 ml-2" />
@@ -364,90 +468,6 @@ export default function ClientPage() {
           </TableBody>
         </Table>
       </div>
-      <Sheet
-        open={payMonitoringSheetOpen}
-        onOpenChange={(open) => {
-          setPayMonitoringSheetOpen(open);
-          !open && setUserId(null);
-        }}
-      >
-        <SheetContent
-          className="md:max-w-[700px] lg:max-w-[900px]"
-          side="right"
-        >
-          <SheetDescription className="hidden">hide</SheetDescription>
-          <SheetHeader>
-            <SheetTitle className="text-left">Mijoz parkovkalari</SheetTitle>
-          </SheetHeader>
-          {userParkingsLoading ? (
-            <div className="h-[200px] flex items-center justify-center">
-              <LoadingComp />
-            </div>
-          ) : (
-            <div className="grid gap-4 py-4">
-              <div>
-                <div className="relative max-h-[70vh] w-full overflow-y-auto rounded-lg border">
-                  <Table>
-                    <TableHeader>
-                      {tableParking.getHeaderGroups().map((headerGroup) => (
-                        <TableRow key={headerGroup.id}>
-                          {headerGroup.headers.map((header) => {
-                            return (
-                              <TableHead className="sticky top-0 z-[1]" key={header.id}>
-                                {header.isPlaceholder
-                                  ? null
-                                  : flexRender(header.column.columnDef.header, header.getContext())}
-                              </TableHead>
-                            );
-                          })}
-                        </TableRow>
-                      ))}
-                    </TableHeader>
-                    <TableBody className="w-full">
-                      {tableParking.getRowModel().rows?.length ? (
-                        tableParking.getRowModel().rows.map((row) => {
-                          return (
-                            <TableRow
-                              key={row.id}
-                              data-state={row.getIsSelected() && "selected"}
-                            >
-                              {row.getVisibleCells().map((cell) => {
-                                return (
-                                  <TableCell key={cell.id}>
-                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                                  </TableCell>
-                                );
-                              })}
-                            </TableRow>
-                          )
-                        })
-                      ) : (
-                        <TableRow className="h-[62vh] w-full">
-                          <TableCell rowSpan={5} colSpan={columns.length} className="h-24 text-center">
-                            Ma'lumot topilmadi.
-                          </TableCell>
-                        </TableRow>
-                      )}
-                    </TableBody>
-                  </Table>
-                </div>
-              </div>
-            </div>
-          )}
-        </SheetContent>
-      </Sheet>
-      <ClientCreate
-        setSheetOpen={setAddSheetOpen}
-        sheetOpen={addSheetOpen}
-        refetch={refetch}
-      />
-      <ClientUpdate
-        setSheetOpen={setEditSheetOpen}
-        sheetOpen={editSheetOpen}
-        refetch={refetch}
-        id={editId}
-        setEditId={setEditId}
-      />
-    </section>
-  );
+    </>
+  )
 }
